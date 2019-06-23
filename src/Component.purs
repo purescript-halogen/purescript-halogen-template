c1e2ae0a667aa610299dd7168a293610044bd86f
LAPTOP-495T50U5\Aaron Snook <aabsnook@gmail.com>
2019-06-23 17:46:39 -0500

    Updating template to line up with Halogen 5.0.0-rc.1

--- src/Component.purs
+++ src/Component.purs
@@ -2,47 +2,45 @@ module Component where
 
 import Prelude
 
+import Data.Const (Const)
 import Data.Maybe (Maybe(..))
-
+import Halogen (ComponentSlot)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 
-data Query a = ToggleState a
+data Action = ToggleState
 
 type State = { on :: Boolean }
 
-component :: forall m. H.Component HH.HTML Query Unit Void m
+component :: forall m. H.Component HH.HTML (Const Void) Unit Void m
 component =
-  H.component
+  H.mkComponent
     { initialState: const initialState
     , render
-    , eval
-    , receiver: const Nothing
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
 
-  initialState :: State
-  initialState = { on: false }
+    initialState :: State
+    initialState = { on: false }
 
-  render :: State -> H.ComponentHTML Query
-  render state =
-    HH.div_
-      [ HH.h1_
-          [ HH.text "Hello world!" ]
-      , HH.p_
-          [ HH.text "Why not toggle this button:" ]
-      , HH.button
-          [ HE.onClick (HE.input_ ToggleState) ]
-          [ HH.text
-              if not state.on
-              then "Don't push me"
-              else "I said don't push me!"
-          ]
-      ]
+    render :: State -> HH.HTML (ComponentSlot HH.HTML () m Action) Action
+    render state =
+      HH.div_
+        [ HH.h1_
+            [ HH.text "Hello world!" ]
+        , HH.p_
+            [ HH.text "Why not toggle this button:" ]
+        , HH.button
+            [ HE.onClick (\_ -> Just ToggleState) ]
+            [ HH.text
+                if not state.on
+                then "Don't push me"
+                else "I said don't push me!"
+            ]
+        ]
 
-  eval :: Query ~> H.ComponentDSL State Query Void m
-  eval = case _ of
-    ToggleState next -> do
-      _ <- H.modify (\state -> { on: not state.on })
-      pure next
+    handleAction :: Action -> H.HalogenM State Action () Void m Unit
+    handleAction = case _ of
+      ToggleState -> void $ H.modify (\state -> { on: not state.on })
